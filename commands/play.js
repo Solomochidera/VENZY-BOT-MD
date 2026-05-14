7b5f03a5a318fdde83869b30d0d7a47db51cd35e
solomonchidera <148859190+Solomochidera@users.noreply.github.com>
2026-05-14 21:21:05 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -59,5 +59,5 @@ async function playCommand(sock, chatId, message) {
 
 module.exports = playCommand; 
 
-/*Powered by KNIGHT-BOT*
-*Credits to Keith MD*`*/
+/*Powered by 𝗩𝗘𝗡𝗭𝗬 𝗕𝗢𝗧-𝗠𝗗*
+*Credits to al†esse*`*/
