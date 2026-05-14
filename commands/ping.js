251688fb7d0be9036e87690dffe9d05378637bb1
solomonchidera <148859190+Solomochidera@users.noreply.github.com>
2026-05-14 21:26:39 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -29,7 +29,7 @@ async function pingCommand(sock, chatId, message) {
         const uptimeFormatted = formatTime(uptimeInSeconds);
 
         const botInfo = `
-┏━━〔 🤖 𝐊𝐧𝐢𝐠𝐡𝐭𝐁𝐨𝐭-𝐌𝐃 〕━━┓
+┏━━〔 👨🏻‍💻 𝗩𝗘𝗡𝗭𝗬 𝗕𝗢𝗧-𝗠𝗗 〕━━┓
 ┃ 🚀 Ping     : ${ping} ms
 ┃ ⏱️ Uptime   : ${uptimeFormatted}
 ┃ 🔖 Version  : v${settings.version}
